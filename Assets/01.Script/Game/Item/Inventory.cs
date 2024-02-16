03284706714dba26d49ee5f01b1b2cbb3ce1b9f3
kimdasan <67263093+Plagem@users.noreply.github.com>
2024-02-16 17:30:02 +0900

    인벤토리 거의 완성

--- Assets/01.Script/Game/Item/Inventory.cs
+++ Assets/01.Script/Game/Item/Inventory.cs
@@ -5,43 +5,102 @@ using UnityEngine;
 
 public enum Item
 {
-    None,
     Item1,
     Item2,
     Item3,
     Item4,
-    Item5
+    Item5,
+    None
 }
 
 public class Inventory : MonoBehaviour
 {
     private int maxItemCount;
     private int itemCount;
-    public Item[] characterItems;
 
+    [SerializeField]
+    private GameObject[] ItemSlot;
+
+    [SerializeField]
+    private GameObject[] ItemPrefebs;
+    
+    public Item[] CharacterItems;
+
+
+    private void Start()
+    {
+        InitInventory();
+    }
 
     // 게임 시작, 게임 오버, 게임 재시작 시 인벤토리를 초기화한다
-    void InitInventory()
+    public void InitInventory()
     {
         Debug.Log("Init Inventory");
         maxItemCount = 5;
         itemCount = 0;
-        characterItems = Enumerable.Repeat(Item.None, maxItemCount).ToArray();
+        CharacterItems = Enumerable.Repeat(Item.None, maxItemCount).ToArray();
     }
 
     // 아이템을 얻는다
-    void GetItem(Item item)
+    private void GetItem(Item item)
     {
-        // 인벤토리가 꽉 찼을 경우
-        if(itemCount >= maxItemCount - 1)
+        Debug.Log("Get Item");
+        Debug.Log(itemCount);
+        if(itemCount < maxItemCount)
         {
-            characterItems[itemCount] = item;
-            itemCount++;
+            for(int i = 0; i < maxItemCount; i++)
+            {
+                if (CharacterItems[i] == Item.None)
+                {
+                    CharacterItems[i] = item;
+                    itemCount++;
+                    break;
+                }
+            }
+
+            DrawInventory();
         }
+        // 인벤토리가 꽉 찼을 경우
+        else
         {
             Debug.Log("Full Inventory - Can't Get Item");
             return;
         }
     }
 
+    private void DrawInventory()
+    {
+        // 아이템 칸 수만큼 For Loop
+        for(int i = 0; i < maxItemCount;i++)
+        {
+            // 현재 아이템 이미지 삭제
+            for(int j = 0; j < ItemSlot[i].transform.childCount; j++)
+            {
+                Destroy(ItemSlot[i].transform.GetChild(0).gameObject);
+            }
+            // 새로운 아이템 이미지 삽입
+            int nowItemNum = (int)CharacterItems[i];
+            GameObject nowDrawingItem = Instantiate(ItemPrefebs[nowItemNum]);
+            nowDrawingItem.transform.SetParent(ItemSlot[i].transform);
+            nowDrawingItem.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    public void UsingItem(int itemSlot)
+    {
+        CharacterItems[itemSlot] = Item.None;
+        itemCount--;
+        DrawInventory();
+    }
+
+
+    public void GetItemTest()
+    {
+        GetItem(0);
+    }
+
+    public void UsingItemTest()
+    {
+        UsingItem(2);
+    }
 }
